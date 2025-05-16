a42f127c53ca757375553914ed3cf466ef0e95c3
fathb <eglass.boy@gmail.com>
2025-05-16 09:55:50 +0700

    v1.0 delete base

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base : "/toko-kita/",
   plugins: [
     vue(),
     vueDevTools(),
